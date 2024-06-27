201e3b35b19d7a6c12b8a9595bff3e72999640c5
João Paulo Borges da Silva <joao.borges@hugol.local>
2024-06-27 18:16:02 -0300

    JAVA

---
+++ estruturas-excepcionais/TryCatch.java
@@ -0,0 +1,39 @@
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * TryCatch
+ */
+public class TryCatch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
+
+            // Imprimindo os dados obtidos pelo usuário
+            System.out.println("Olá, me chamo: " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho: " + idade + " anos");
+            System.out.println("Minha altura é : " + altura + "cm ");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        } finally {
+            // Fechando o Scanner para liberar os recursos
+            scanner.close();
+        }
+    }
+}
